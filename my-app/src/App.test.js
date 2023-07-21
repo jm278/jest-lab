9aa2093fa4b6d28c9fa657dcf1836b9001e9806e
Jennifer Morales <jm278@uw.edu>
2023-07-20 18:14:16 -0700

    add jest

--- my-app/src/App.test.js
+++ my-app/src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import userEvent from "@testing-library/user-event"
 
-test('renders learn react link', () => {
+
+  test('Class: checking checkbox functionality', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  //check if box is not checked 
+  userEvent.click(screen.getByTestId("checkbox"));
+  expect(screen.getByTestId("checkbox")).toBeChecked()
+});
+
+  test("alone:check if 'Hello World' is on page", () => {
+   // render(<App />);
+    const { getByText } = render(<App />);
+    const headingElement = getByText("Hello World");
+    expect(headingElement.tagName).toBe("H1");
 });
